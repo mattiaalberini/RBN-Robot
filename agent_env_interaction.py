1db88fca05440e88c5bb81acd9e5d022a29219ce
Mattia <mattiaalberini22@gmail.com>
2025-06-22 14:29:13 +0200

    Add 3 modes

--- agent_env_interaction.py
+++ agent_env_interaction.py
@@ -3,6 +3,39 @@ import os
 from utils import read_graph, read_initconditions, print_states, simulate_step
 
 
+# Lettore parametri da file
+def read_file(file_name):
+    parameters_app = {}
+
+    with open(file_name, "r", encoding="utf-8") as file:
+        next_read = ""
+
+        for line in file:
+            if ":" in line:
+                next_read = ""
+                parameters_app[line.split(":")[0].strip()] = line.split(":")[1].strip()
+            elif "EFFETTORI" in line:
+                next_read = "effettori"
+                parameters_app["effettori"] = {}
+            elif "SENSORI" in line:
+                next_read = "sensori"
+                parameters_app["sensori"] = {}
+
+            if next_read == "effettori":
+                parts = line.split()
+                if len(parts) == 2:
+                    agent, env = map(int, parts)
+                    parameters_app["effettori"][agent] = env
+            elif next_read == "sensori":
+                parts = line.split()
+                if len(parts) == 2:
+                    agent, env = map(int, parts)
+                    parameters_app["sensori"][env] = agent
+
+
+    return parameters_app
+
+
 # Controllore ammissibilità parametri
 def check_parameters(agent_ncond, env_ncond):
     # Agente e ambiente devono avere lo stesso numero di condizioni iniziali
@@ -10,63 +43,82 @@ def check_parameters(agent_ncond, env_ncond):
         raise ValueError("Numero condizioni iniziali agente e ambiente diverso")
 
 
-def simulate_agent_env_steps(agent_rbn, env_rbn, tot_steps, agent_step, env_step, soglia_sensori, soglia_effettori, effettori, sensori, agent_state, env_state, agent_node_val_sum, env_node_val_sum):
-    agent_states, env_states = [], []
-    agent_states.append(list(agent_state))
-    env_states.append(list(env_state))
+def simulate_agent_env_step (s, agent_rbn, env_rbn, agent_step, env_step, soglia_sensori, soglia_effettori, effettori, sensori, agent_state, env_state, agent_node_val_sum, env_node_val_sum):
+    # Tengo traccia dei valori dei nodi effettori e dei nodi letti dai sensori
+    # Nodi effettori
+    for a in agent_node_val_sum:
+        agent_node_val_sum[a] += agent_state[a]
+    # Nodi letti dai sensori
+    for e in env_node_val_sum:
+        env_node_val_sum[e] += env_state[e]
 
-    # Inizializzo a 0
+    if s % agent_step == 0:
+        # L'agente si modifica
+        print("Modifica agente")
+        for e in env_node_val_sum:
+            if env_node_val_sum[e] / agent_step < soglia_sensori:
+                # Media < Soglia -> Le uscite del nodo saranno tutte 0 e il nodo sarà = 0
+                agent_rbn[sensori[e]]["uscite"] = [0 for _ in agent_rbn[sensori[e]]["uscite"]]
+                agent_state[sensori[e]] = 0
+                print("Nodo " + str(sensori[e]) + " -> 0 (preso da nodo " + str(e) + " dell'ambiente)")
+            else:
+                # Media < Soglia -> Le uscite del nodo saranno tutte 1 e il nodo sarà = 1
+                agent_rbn[sensori[e]]["uscite"] = [1 for _ in agent_rbn[sensori[e]]["uscite"]]
+                agent_state[sensori[e]] = 1
+                print("Nodo " + str(sensori[e]) + " -> 1 (preso da nodo " + str(e) + " dell'ambiente)")
+
+        agent_state = simulate_step(agent_state, agent_rbn)
+        env_node_val_sum = {x: 0 for x in env_node_val_sum}
+
+    if s % env_step == 0:
+        # L'agente modifica l'ambiente
+        print("Modifica ambiente")
+        for e in agent_node_val_sum:
+            if agent_node_val_sum[e] / env_step < soglia_effettori:
+                # Media < Soglia -> Le uscite del nodo saranno tutte 0 e il nodo sarà = 0
+                env_rbn[effettori[e]]["uscite"] = [0 for _ in env_rbn[effettori[e]]["uscite"]]
+                env_state[effettori[e]] = 0
+                print("Nodo " + str(effettori[e]) + " -> 0 (preso da nodo " + str(e) + " dell'agente)")
+            else:
+                # Media < Soglia -> Le uscite del nodo saranno tutte 1 e il nodo sarà = 1
+                env_rbn[effettori[e]]["uscite"] = [1 for _ in env_rbn[effettori[e]]["uscite"]]
+                env_state[effettori[e]] = 1
+                print("Nodo " + str(effettori[e]) + " -> 1 (preso da nodo " + str(e) + " dell'agente)")
+
+        env_state = simulate_step(env_state, env_rbn)
+        agent_node_val_sum = {x: 0 for x in agent_node_val_sum}
+
+    return agent_state, env_state, agent_node_val_sum, env_node_val_sum
+
+
+# Mode 1: solo stato finale
+def simulate_agent_env_steps_mode1(agent_rbn, env_rbn, tot_steps, agent_step, env_step, soglia_sensori, soglia_effettori, effettori, sensori, agent_state, env_state, agent_node_val_sum, env_node_val_sum):
+
+    # Variabile temporanea per calcolare la media dei valori che interagiscono con l'altra RBN
     agent_node_val_sum = {x: 0 for x in agent_node_val_sum}
     env_node_val_sum = {x: 0 for x in env_node_val_sum}
 
     for s in range(1, tot_steps):
         print(s)
-        # Tengo traccia dei valori dei nodi effettori e dei nodi letti dai sensori
-        # Nodi effettori
-        for a in agent_node_val_sum:
-            agent_node_val_sum[a] += agent_state[a]
-        # Nodi letti dai sensori
-        for e in env_node_val_sum:
-            env_node_val_sum[e] += env_state[e]
+        agent_state, env_state, agent_node_val_sum, env_node_val_sum = simulate_agent_env_step(s, agent_rbn, env_rbn, agent_step, env_step, soglia_sensori, soglia_effettori, effettori, sensori, agent_state, env_state, agent_node_val_sum, env_node_val_sum)
+        print("\n")
 
-        if s % agent_step == 0:
-            # L'agente si modifica
-            print("Modifica agente")
-            for e in env_node_val_sum:
-                if env_node_val_sum[e] / agent_step < soglia_sensori:
-                    # Media < Soglia -> Le uscite del nodo saranno tutte 0 e il nodo sarà = 0
-                    agent_rbn[sensori[e]]["uscite"] = [0 for _ in agent_rbn[sensori[e]]["uscite"]]
-                    agent_state[sensori[e]] = 0
-                    print("Nodo " + str(sensori[e]) + " -> 0 (preso da nodo " + str(e) + " dell'ambiente)")
-                else:
-                    # Media < Soglia -> Le uscite del nodo saranno tutte 1 e il nodo sarà = 1
-                    agent_rbn[sensori[e]]["uscite"] = [1 for _ in agent_rbn[sensori[e]]["uscite"]]
-                    agent_state[sensori[e]] = 1
-                    print("Nodo " + str(sensori[e]) + " -> 1 (preso da nodo " + str(e) + " dell'ambiente)")
+    return agent_state, env_state
 
-            agent_state = simulate_step(agent_state, agent_rbn)
 
-            env_node_val_sum = {x: 0 for x in env_node_val_sum}
+# Mode 2: tutti gli stati
+def simulate_agent_env_steps_mode2(agent_rbn, env_rbn, tot_steps, agent_step, env_step, soglia_sensori, soglia_effettori, effettori, sensori, agent_state, env_state, agent_node_val_sum, env_node_val_sum):
+    agent_states, env_states = [], []
+    agent_states.append(list(agent_state))
+    env_states.append(list(env_state))
 
-        if s % env_step == 0:
-            # L'agente modifica l'ambiente
-            print("Modifica ambiente")
-            for e in agent_node_val_sum:
-                if agent_node_val_sum[e] / env_step < soglia_effettori:
-                    # Media < Soglia -> Le uscite del nodo saranno tutte 0 e il nodo sarà = 0
-                    env_rbn[effettori[e]]["uscite"] = [0 for _ in env_rbn[effettori[e]]["uscite"]]
-                    env_state[effettori[e]] = 0
-                    print("Nodo " + str(effettori[e]) + " -> 0 (preso da nodo " + str(e) + " dell'agente)")
-                else:
-                    # Media < Soglia -> Le uscite del nodo saranno tutte 1 e il nodo sarà = 1
-                    env_rbn[effettori[e]]["uscite"] = [1 for _ in env_rbn[effettori[e]]["uscite"]]
-                    env_state[effettori[e]] = 1
-                    print("Nodo " + str(effettori[e]) + " -> 1 (preso da nodo " + str(e) + " dell'agente)")
+    # Variabile temporanea per calcolare la media dei valori che interagiscono con l'altra RBN
+    agent_node_val_sum = {x: 0 for x in agent_node_val_sum}
+    env_node_val_sum = {x: 0 for x in env_node_val_sum}
 
-            env_state = simulate_step(env_state, env_rbn)
-
-            agent_node_val_sum = {x: 0 for x in agent_node_val_sum}
-
+    for s in range(1, tot_steps):
+        print(s)
+        agent_state, env_state, agent_node_val_sum, env_node_val_sum = simulate_agent_env_step(s, agent_rbn, env_rbn, agent_step, env_step, soglia_sensori, soglia_effettori, effettori, sensori, agent_state, env_state, agent_node_val_sum, env_node_val_sum)
         agent_states.append(list(agent_state))
         env_states.append(list(env_state))
         print("\n")
@@ -74,34 +126,56 @@ def simulate_agent_env_steps(agent_rbn, env_rbn, tot_steps, agent_step, env_step
     return agent_states, env_states
 
 
+# Mode 3: ogni stato al cambiamento
+def simulate_agent_env_steps_mode3(agent_rbn, env_rbn, tot_steps, agent_step, env_step, soglia_sensori, soglia_effettori, effettori, sensori, agent_state, env_state, agent_node_val_sum, env_node_val_sum):
+    agent_states, env_states = [], []
+    agent_states.append(list(agent_state))
+    env_states.append(list(env_state))
+
+    # Variabile temporanea per calcolare la media dei valori che interagiscono con l'altra RBN
+    agent_node_val_sum = {x: 0 for x in agent_node_val_sum}
+    env_node_val_sum = {x: 0 for x in env_node_val_sum}
+
+    for s in range(1, tot_steps):
+        print(s)
+        agent_state, env_state, agent_node_val_sum, env_node_val_sum = simulate_agent_env_step(s, agent_rbn, env_rbn, agent_step, env_step, soglia_sensori, soglia_effettori, effettori, sensori, agent_state, env_state, agent_node_val_sum, env_node_val_sum)
+
+        if s % agent_step == 0:
+            agent_states.append(list(agent_state))
+
+        if s % env_step == 0:
+            env_states.append(list(env_state))
+
+        print("\n")
+
+    return agent_states, env_states
+
 
 def main():
+
+    parameters = read_file("input_AG_AMB.txt")
+    print(parameters)
+
     agent_num_genes, agent_rbn = read_graph(os.path.join("agent", "grafo_default.txt"))
     env_num_genes, env_rbn = read_graph(os.path.join("environment", "grafo_default.txt"))
 
     agent_ncond, agent_initconditions = read_initconditions(os.path.join("agent", "cond_default.txt"))
     env_ncond, env_initconditions = read_initconditions(os.path.join("environment", "cond_default.txt"))
 
-    tot_steps = 20
-    agent_step = 3
-    env_step = 5
+    tot_steps = int(parameters["n_steps"])
+    agent_step = int(parameters["orologio agente"])
+    env_step = int(parameters["orologio ambiente"])
 
-    soglia_sensori = 0.9 # Ambiente che modifica l'agente
-    soglia_effettori = 0.9 # Agente che modifica l'ambiente
+    soglia_sensori = float(parameters["soglia sensori"]) # Ambiente che modifica l'agente
+    soglia_effettori = float(parameters["soglia effettori"]) # Agente che modifica l'ambiente
 
     # ID NODO AGENTE - ID NODO AMBIENTE
-    effettori = {
-        2: 14,
-        13: 3
-    }
-
-    # Ho invertito l'ordine rispetto al file
+    effettori = parameters["effettori"]
     # ID NODO AMBIENTE - ID NODO AGENTE
-    sensori = {
-        15: 12,
-        0: 1,
-        12: 2
-    }
+    sensori = parameters["sensori"]
+
+    # Mode
+    mode = int(parameters["mode"])
 
     # Controllo la correttezza dei parametri
     check_parameters(agent_ncond, env_ncond)
@@ -110,17 +184,42 @@ def main():
     agent_node_val_sum = {x: 0 for x in effettori}
     env_node_val_sum = {x: 0 for x in sensori}
 
-    agent_states, env_states = [], []
+    agent_final_states, env_final_states = [], []
 
-    for c in range(agent_ncond):
+    # Mode 1: stampo solo stato finale
+    if mode == 1:
+        for c in range(agent_ncond):
+            agent_final_state, env_final_state = simulate_agent_env_steps_mode1(agent_rbn, env_rbn, tot_steps, agent_step, env_step, soglia_sensori, soglia_effettori, effettori, sensori, agent_initconditions[c], env_initconditions[c], agent_node_val_sum, env_node_val_sum)
 
-        agent_state = agent_initconditions[c]
-        env_state = env_initconditions[c]
+            print(agent_final_state)
+            print(env_final_state)
 
-        agent_states, env_states = simulate_agent_env_steps(agent_rbn, env_rbn, tot_steps, agent_step, env_step, soglia_sensori, soglia_effettori, effettori, sensori, agent_state, env_state, agent_node_val_sum, env_node_val_sum)
+            agent_final_states.append(agent_final_state)
+            env_final_states.append(env_final_state)
 
-    print_states(agent_num_genes, agent_ncond, agent_states, os.path.join("agent", "output_interaction.txt"))
-    print_states(env_num_genes, env_ncond, env_states, os.path.join("environment", "output_interaction.txt"))
+    # Mode 2: stampo tutti gli stati
+    elif mode == 2:
+        for c in range(agent_ncond):
+            agent_states, env_states = simulate_agent_env_steps_mode2(agent_rbn, env_rbn, tot_steps, agent_step, env_step, soglia_sensori, soglia_effettori, effettori, sensori, agent_initconditions[c], env_initconditions[c], agent_node_val_sum, env_node_val_sum)
+
+            for a in agent_states:
+                agent_final_states.append(a)
+            for e in env_states:
+                env_final_states.append(e)
+
+    # Mode 3: stampo ogni stato al cambiamento
+    elif mode == 3:
+        for c in range(agent_ncond):
+            agent_states, env_states = simulate_agent_env_steps_mode3(agent_rbn, env_rbn, tot_steps, agent_step, env_step, soglia_sensori, soglia_effettori, effettori, sensori, agent_initconditions[c], env_initconditions[c], agent_node_val_sum, env_node_val_sum)
+
+            for a in agent_states:
+                agent_final_states.append(a)
+            for e in env_states:
+                env_final_states.append(e)
+
+
+    print_states(agent_num_genes, agent_ncond, agent_final_states, os.path.join("agent", "output_interaction.txt"))
+    print_states(env_num_genes, env_ncond, env_final_states, os.path.join("environment", "output_interaction.txt"))
 
 
 if __name__ == "__main__":
