10a19af7daa70e3c6a77aebfa43e6e7eb810519f
Mattia <mattiaalberini22@gmail.com>
2025-08-13 21:51:15 +0200

    Benessere interaction simulator

--- benessere_interaction_simulator.py
+++ benessere_interaction_simulator.py
@@ -1,7 +1,17 @@
+import copy
 import os
 import subprocess
+import math
 
-from utils import print_states
+from generator_RBN import print_grafo
+from utils import print_states, read_file, read_graph, generate_random_boolean_values
+
+
+def check_parameters(n_iterazioni, mode):
+    if n_iterazioni < 1:
+        raise ValueError("Valore numero iterazioni non accettabile!")
+    if mode != 'a' and mode != 'b':
+        raise ValueError("Valore mode diverso da 'a' e 'b'!")
 
 
 def read_init_condition(file_name):
@@ -73,15 +83,79 @@ def write_input_benessere_file(file_name, agent_periodo, nodi_essenziali, valori
     with open(file_name, "w") as file:
         file.write(f"omega: {agent_periodo}\n")
         file.write("ELENCO NODI ESSENZIALI (ID NODO AGENTE - VALORE IDEALE)\n")
-        print(valori_ideali)
 
         for e in nodi_essenziali:
             file.write(f"{e}  {valori_ideali[e]}\n")
 
 
+def read_benessere(file_name):
+    with open(file_name, "r", encoding="utf-8") as file:
+        benessere = file.readline().strip()
+        return benessere
+
+
+def write_output_benessere_file(file_name, benesseri):
+    with open(file_name, "w") as file:
+        for b in benesseri:
+            file.write(f"{b["benessere"]}   ")
+            for funzione in b["funzioni"]:
+                for x in funzione:
+                    file.write(f"{x}")
+                file.write(f" ")
+            file.write(f"\n")
+
+
+def ricalcola_uscite_graph(n_uscite, bias_per_ogni_k, k_minimo):
+    bias = bias_per_ogni_k[int(math.log2(n_uscite)) - k_minimo]
+    new_uscite = generate_random_boolean_values(n_uscite, bias)
+    return new_uscite
+
+
+def genera_funzioni_booleane(rbn_agent, nodi_essenziali, bias_per_ogni_k_agente, k_minimo):
+    new_funzioni_nodi_essenziali = []
+
+    for e in nodi_essenziali:
+        new_uscite = ricalcola_uscite_graph(len(rbn_agent[e]["uscite"]), bias_per_ogni_k_agente, k_minimo)
+        new_funzioni_nodi_essenziali.append(new_uscite)
+
+    return new_funzioni_nodi_essenziali
+
+
+def check_funzioni_booleane_uguali(benesseri, new_funzioni_nodi_essenziali):
+    for b in benesseri:
+        if b["funzioni"] == new_funzioni_nodi_essenziali:
+            return True
+    return False
+
+
+def read_last_state(file_name):
+    init_conditions = []
+
+    last_state = None
+    with open(file_name, "r") as f:
+        for row in f:
+            clean_row = row.strip()
+            if clean_row:
+                last_state = clean_row
+
+    init_condition = [int(x) for x in last_state.strip().split()]
+    init_conditions.append(init_condition)
+    return init_conditions
+
+
 def main():
+    parameters = read_file("benessere_interaction_simulator_input.txt")
+    n_iterazioni = int(parameters["n_iterazioni"])
+    mode = parameters["mode"].lower()
+
+    check_parameters(n_iterazioni, mode)
+
+    agent_n_genes, rbn_agent = read_graph(os.path.join("agent", "grafo_default.txt"))
+
+    rbn_agent_iniziale = copy.deepcopy(rbn_agent)
+
     # Genero le condizioni iniziali
-    subprocess.run(["python", "generator_initconditions.py", "-a", "-e"])
+    #subprocess.run(["python", "generator_initconditions.py", "-a", "-e"])
 
     # Simulo il comportamento dell'agente e dell'ambiente in mode 3
     subprocess.run(["python", "simulator.py", "-a", "-e"])
@@ -107,8 +181,60 @@ def main():
     # Scrivo il omega = periodo e il valore ideale dei nodi essenziali
     write_input_benessere_file("input_benessere.txt", agent_periodo, nodi_essenziali, valori_ideali)
 
-    # Simulo interazione tra agente e ambiente -> calcola direttamente il benessere se in mode 2
-    subprocess.run(["python", "agent_env_interaction.py", "-a", "-e"])
+    # Lettura parametri necessari per riscrivere le funzioni booleane
+    parameters_rbn_generator = read_file(os.path.join("agent", "input_generatore.txt"))
+    bias_per_ogni_k_agente = list(map(float, parameters_rbn_generator["bias_per_ogni_k"].split()))
+    k_minimo = int(parameters_rbn_generator["k_minimo"])
+
+    benesseri = []
+
+    for i in range(n_iterazioni):
+        # Simulo interazione tra agente e ambiente -> calcola direttamente il benessere se in mode 2
+        subprocess.run(["python", "agent_env_interaction.py", "-a", "-e"])
+
+        funzioni_nodi_essenziali = []
+        benessere = read_benessere("benessere_agent.txt")
+
+        # Memorizzo il valore delle ultime funzioni booleane
+        for e in nodi_essenziali:
+            funzioni_nodi_essenziali.append(rbn_agent[e]["uscite"])
+
+        iterazione = {"benessere": benessere, "funzioni": funzioni_nodi_essenziali}
+        benesseri.append(iterazione)
+
+        # Genero nuove funzioni booleane
+        new_funzioni_nodi_essenziali = genera_funzioni_booleane(rbn_agent, nodi_essenziali, bias_per_ogni_k_agente, k_minimo)
+
+        c = 0
+        ciclo_infinito = False
+        # Controllo che non sia già stata generata una funzione booleana uguale
+        while check_funzioni_booleane_uguali(benesseri, new_funzioni_nodi_essenziali):
+            new_funzioni_nodi_essenziali = genera_funzioni_booleane(rbn_agent, nodi_essenziali, bias_per_ogni_k_agente, k_minimo)
+            c += 1
+            if c > 500:
+                ciclo_infinito = True
+                break
+
+        if ciclo_infinito:
+            break
+
+        for j, e in enumerate(nodi_essenziali):
+            rbn_agent[e]["uscite"] = new_funzioni_nodi_essenziali[j]
+
+        print_grafo(agent_n_genes, rbn_agent, "agent")
+
+        if mode == "b": # Condizione iniziale = ultimo stato raggiunto
+            agent_last_state = (read_last_state(os.path.join("agent", "output_interaction_mode3.txt")))
+            env_last_state = (read_last_state(os.path.join("environment", "output_interaction_mode3.txt")))
+
+            # Scrivo le nuove condizioni iniziali su file
+            print_states(agent_n_genes, 1, agent_last_state, os.path.join("agent", "cond_default.txt"))
+            print_states(env_n_genes, 1, env_last_state, os.path.join("environment", "cond_default.txt"))
+
+    write_output_benessere_file("benessere_interaction_simulator_output.txt", benesseri)
+
+    # Ripristino la RBN iniziale
+    print_grafo(agent_n_genes, rbn_agent_iniziale, "agent")
 
 
 if __name__ == "__main__":
