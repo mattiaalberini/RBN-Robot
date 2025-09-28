b20aa55b5ac08a84c4b3c389c97f4e5b5a081329
Mattia <mattiaalberini22@gmail.com>
2025-09-28 21:41:57 +0200

    Evoluzione

--- evolution.py
+++ evolution.py
@@ -1,11 +1,11 @@
 import os
+import random
 import shutil
 import subprocess
 from decimal import Decimal
-import pandas
-import numpy
 
-from utils import read_file
+from generator_RBN import print_grafo
+from utils import read_file, read_graph
 
 
 # Crea la cartella che conterrà le generazioni
@@ -48,8 +48,123 @@ def read_valore_ideale_nodi_essenziali(file_name):
     return nodi_essenziali
 
 
-def find_best_benessere():
-    return -1
+def find_best_benessere(file):
+    first_row = True
+
+    with open(file, "r") as f:
+        for linea in f:
+            colonne = linea.strip().split()
+            if not colonne:
+                continue
+            benessere = float(colonne[0])  # Prima colonna (benessere)
+            funzioni_booleane = colonne[1:]  # Altre colonne (funzioni booleane)
+
+            if first_row:
+                best_benessere = benessere
+                best_funzioni_booleane = funzioni_booleane
+                first_row = False
+            elif benessere < best_benessere:
+                best_benessere = benessere
+                best_funzioni_booleane = funzioni_booleane
+
+    return best_benessere, best_funzioni_booleane
+
+
+def read_nodi_effettori(file_name):
+    effettori = []
+    effettori_ambiente = []
+
+    with open(file_name, "r", encoding="utf-8") as file:
+        next_read = ""
+
+        for line in file:
+            if ":" in line or "SENSORI" in line:
+                next_read = ""
+            elif "EFFETTORI" in line:
+                next_read = "effettori"
+
+            if next_read == "effettori":
+                parts = line.split()
+                if len(parts) == 2:
+                    agent, env = map(int, parts)
+                    effettori.append(agent)
+                    effettori_ambiente.append(env)
+
+    return effettori, effettori_ambiente
+
+
+def write_nodi_effettori(file_name, effettori_agente, effettori_ambiente):
+    new_righe = []
+
+    print(effettori_agente, effettori_ambiente)
+
+    with open(file_name, "r") as file:
+        in_effettori = False
+        i = -1
+
+        for line in file:
+            if "EFFETTORI" in line:
+                in_effettori = True
+            elif "SENSORI" in line:
+                in_effettori = False
+
+            if in_effettori:
+                if i >= 0:
+                    coppia = str(effettori_agente[i]) + "\t" + str(effettori_ambiente[i]) + "\n"
+                    new_righe.append(coppia)
+                else:
+                    new_righe.append(line)
+                i += 1
+            else:
+                new_righe.append(line)
+
+    with open(file_name, "w") as f:
+        f.writelines(new_righe)
+
+
+def generazione(dir_lanci, i):
+    # Creo la cartella della generazione
+    dir_lancio = os.path.join(os.getcwd(), dir_lanci, f"G{i}")
+    os.mkdir(dir_lancio)
+
+    # Eseguo la simulazione
+    subprocess.run(["python", "benessere_interaction_simulator.py"])
+    best_benessere, best_funzioni_booleane = find_best_benessere("benessere_interaction_simulator_output.txt")
+
+    # Copio i file all'interno della cartella del relativo lancio
+    shutil.copytree(os.path.join(os.getcwd(), "agent"), os.path.join(dir_lancio, "agent"), dirs_exist_ok=True)
+    shutil.copytree(os.path.join(os.getcwd(), "environment"), os.path.join(dir_lancio, "environment"),
+                    dirs_exist_ok=True)
+
+    for file in os.listdir(os.getcwd()):
+        if file.endswith(".txt") and os.path.isfile(os.path.join(os.getcwd(), file)):
+            shutil.copy2(os.path.join(os.getcwd(), file), dir_lancio)
+
+    return best_benessere, best_funzioni_booleane
+
+
+# Modifica le funzioni booleane dei nodi effettori dell'agente e il nodo dell'ambiente su cui agisce
+def modifica_agente(best_funzioni_booleane):
+    agent_n_genes, rbn_agent = read_graph(os.path.join("agent", "grafo_default.txt"))
+    env_n_genes, rbn_env = read_graph(os.path.join("environment", "grafo_default.txt"))
+
+    effettori_agente, effettori_ambiente = read_nodi_effettori("input_AG_AMB.txt")
+
+    print(effettori_agente, effettori_ambiente)
+
+    pos_nodo_effettore = random.randint(0, len(effettori_ambiente) - 1)  # Quale nodo effettore modificare
+    new_nodo_ambiente = random.randint(0, int(env_n_genes) - 1)  # Quale nodo dell'ambiente deve toccare
+    while new_nodo_ambiente in effettori_ambiente:
+        new_nodo_ambiente = random.randint(0, int(env_n_genes) - 1)
+
+    effettori_ambiente[pos_nodo_effettore] = new_nodo_ambiente
+
+    write_nodi_effettori("input_AG_AMB.txt", effettori_agente, effettori_ambiente)
+
+    for j, e in enumerate(effettori_agente):
+        rbn_agent[e]["uscite"] = best_funzioni_booleane[j]
+
+    print_grafo(int(agent_n_genes), rbn_agent, "agent")
 
 
 def main():
@@ -63,24 +178,30 @@ def main():
     # Per memorizzare i dati per il file excel
     dati = []
 
-    i = 0
-    benessere_zero = False
+    # Genero il padre (G0)
+    best_benessere, best_funzioni_booleane = generazione(dir_lanci, 0)
+    modifica_agente(best_funzioni_booleane)
+
+    if best_benessere == 0:
+        benessere_zero = True
+    else:
+        benessere_zero = False
+
+    i = 1
+
     while i < n_generazioni and not benessere_zero:
-        # Creo la cartella della generazione
-        dir_lancio = os.path.join(os.getcwd(), dir_lanci, f"G{i}")
-        os.mkdir(dir_lancio)
 
-        # Eseguo la simulazione
-        subprocess.run(["python", "benessere_interaction_simulator.py"])
 
-        # Copio i file all'interno della cartella del relativo lancio
-        shutil.copytree(os.path.join(os.getcwd(), "agent"), os.path.join(dir_lancio, "agent"), dirs_exist_ok=True)
-        shutil.copytree(os.path.join(os.getcwd(), "environment"), os.path.join(dir_lancio, "environment"), dirs_exist_ok=True)
 
-        for file in os.listdir(os.getcwd()):
-            if file.endswith(".txt") and os.path.isfile(os.path.join(os.getcwd(), file)):
-                shutil.copy2(os.path.join(os.getcwd(), file), dir_lancio)
+        if best_benessere == 0:
+            benessere_zero = True
 
+
+        if best_benessere < benessere_padre:
+
+
+            benessere_padre = best_benessere
+            print("figlio migliore")
 
         i += 1
 
